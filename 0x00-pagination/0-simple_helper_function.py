b78223965d4f080a2d4735aecf439f04e9ac6d2d
jabir254 <jabirkullow@gmail.com>
2023-10-22 20:28:33 +0300

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 '''Simple helper functions'''
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''return a tuple containing a start and end index'''
     if page <= 0 or page_size <= 0:
