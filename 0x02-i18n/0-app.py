dd4585fbd8bcf37e26251133ec1a71cd754b13d5
jabir254 <jabirkullow@gmail.com>
2023-11-05 20:04:43 +0300

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,10 +4,10 @@
 from flask import Flask , render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/', strict_slashes=False)
+def index() -> str:
     '''render index.html from templates folder'''
-    return render_template("index.html")
+    return render_template("0-index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port="5000")
